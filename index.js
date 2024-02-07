bc2b36e5cd439959b3c8f704e2ffc0f4c4ce3a54
Facundo Gaidano <41210748+facundogaidano@users.noreply.github.com>
2024-02-07 16:20:19 -0300

    Fix findByIdAndDelete

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
+  Note.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
     })
